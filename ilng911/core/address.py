09768655db036cc969a14a33f75bc9a7b28f6163
Caleb Mackey <caleb.mackey@gmail.com>
2021-05-24 08:58:14 -0500

    feat(core/address): merge street attributes and location info

--- ilng911/core/address.py
+++ ilng911/core/address.py
@@ -1,14 +1,34 @@
+import os
 import arcpy
 from ..env import ng911_db
-from typing import Union
-from ..core.common import Feature, FeatureHelper
-from ..schemas import DataType
+from ..support.munch import munchify, Munch
+from typing import Union, Dict, List
+from ..core.common import Feature
+from ..core.geometry import get_angle
+from ..schemas import DataType, DataSchema
 from warnings import warn
+from itertools import zip_longest
+from .fields import FIELDS
 
+
+thisDir = os.path.abspath(os.path.dirname(__file__))
+
+
+FALLBACK_ZIP_CODES = os.path.join(os.path.dirname(thisDir), 'data', 'il_zip_codes.shp')
 
 STREET_ATTRIBUTES = [
-    'St_PreMod', 'St_PreDir', 'St_PreTyp', 'St_PreSep', 'St_Name', 'St_PosTyp', 
-    'St_PosDir', 'St_PosMod', 'LSt_PreDir', 'LSt_Name', 'LSt_Type', 'LSt_PosDir'
+    'St_PreMod', 
+    'St_PreDir', 
+    'St_PreTyp', 
+    'St_PreSep', 
+    'St_Name', 
+    'St_PosTyp', 
+    'St_PosDir', 
+    'St_PosMod', 
+    'LSt_PreDir', 
+    'LSt_Name', 
+    'LSt_Type', 
+    'LSt_PosDir'
 ]
 
 ADDRESS_ATTRIBUTES = [
@@ -32,6 +52,7 @@ ADDRESS_ATTRIBUTES = [
 ]
 
 SKIP_NAMES = [
+    'DateUpdate',
     "DiscrpAgID",
     "RCL_NGUID",
     "Site_NGUID",
@@ -47,9 +68,7 @@ SKIP_NAMES = [
     "County",
     'Long', 
     'Lat', 
-    'Elev', 
-    'GC_Address_wZIP', 
-    'GC_Address_wMSAGCOMM'
+    'Elev'
 ]
 
 DATE_FIELDS = [
@@ -59,22 +78,31 @@ DATE_FIELDS = [
 ]
 
 def merge_street_segment_attributes(address: Feature, centerline: Union[int, Feature]):
-    centerlineTab = ng911_db.get_911_table(DataType.ROAD_CENTERLINE)
-    centerlineSchema = ng911_db.load_911_schema(DataType.ROAD_CENTERLINE)
+    """merge street segment attributes into address point feature
+
+    Args:
+        address (Feature): an address Feature
+        centerline (Union[int, Feature]): a centerline OBJECTID or Feature
+
+    Returns:
+        Feature: the address Feature
+    """
+    centerlineSchema = DataSchema(DataType.ROAD_CENTERLINE)
+    centerlineTab = centerlineSchema.table
 
     # get matching fields
     fields = arcpy.ListFields(centerlineTab)
     matchFields = STREET_ATTRIBUTES[:]
     for f in fields:
         # also add any matching custom fields?
-        if f.name not in (SKIP_NAMES + DATE_FIELDS + STREET_ATTRIBUTES) and f.name in address.fieldNames:
+        if f.name not in (SKIP_NAMES + DATE_FIELDS + STREET_ATTRIBUTES) and f.name in address.fieldNames and f.editable:
             matchFields.append(f.name)
 
     # if OBJECTID is provided for centerline, fetch attributes and merge
     if isinstance(centerline, int):
         oidField = [f.name for f in fields if f.type == 'OID'][0]
         where = f'{oidField} = {centerline}'
-        
+        print(f'matchFields: {matchFields}')
         with arcpy.da.SearchCursor(centerlineTab, matchFields, where) as rows:
             try:
                 row = [r for r in rows][0]
@@ -89,25 +117,113 @@ def merge_street_segment_attributes(address: Feature, centerline: Union[int, Fea
 
     return address
 
-    
-        
-
-
-
-def get_zip_code(pt: arcpy.Point) -> str:
+def get_zip_code(pt: arcpy.PointGeometry) -> Dict[str, str]:
     """gets a zip code from a given point, using authoritative data from the USPS
 
     see: https://www.arcgis.com/home/item.html?id=8d2012a2016e484dafaac0451f9aea24
 
     Args:
-        pt (arcpy.Point): an arcpy Point geometry
+        pt (arcpy.PointGeometry): an arcpy Point geometry
 
     Returns:
         str: the intersecting zip code used by the USPS
     """
-    pass
+    zipLyr = ng911_db.get_911_layer(DataType.ZIP_CODES)
+    arcpy.management.SelectLayerByLocation(zipLyr, 'INTERSECT', pt)
+    count = arcpy.management.GetCount(zipLyr).getOutput(0)
+    if count:
+        # get zip code and zip code 4
+        with arcpy.da.SearchCursor(zipLyr, ['ZipCode', 'ZipCode4']) as rows:
+            for row in rows:
+                return dict(zip(['Post_Code', 'Post_Code4'], row))
 
-# def extract_street_info(oid: number)
+    # use fallback
+    fallback = arcpy.management.MakeFeatureLayer(FALLBACK_ZIP_CODES).getOutput(0)
+    print('using fallback zip codes', fallback, fallback.dataSource)
+    
+    arcpy.management.SelectLayerByLocation(fallback, 'INTERSECT', pt)
+    with arcpy.da.SearchCursor(fallback, ['ZIP_CODE']) as rows:
+        for row in rows:
+            return dict(zip(['Post_Code', 'Post_Code4'], row + (None,)))
+
+    return { 'Post_Code': None, 'Post_Code4': None }
+
+def get_city_limits(pt: arcpy.PointGeometry) -> Dict[str, str]:
+    """gets the city limits for a given point
+
+    Args:
+        pt (arcpy.PointGeometry): [description]
+
+    Returns:
+        Dict[str, str]: [description]
+    """
+    incLyr = ng911_db.get_911_layer(DataType.INCORPORATED_MUNICIPAL)
+    uniLyr = ng911_db.get_911_layer(DataType.UNINCORPORATED_MUNICIPAL)
+    attrs = dict(
+        Inc_Muni='UNINCORPORATED',
+        Uninc_Comm=None
+    )
+
+    types = [(incLyr, 'Inc_Muni'), (uniLyr, 'Uninc_Comm')]
+    for lyr, field in types:
+        arcpy.management.SelectLayerByLocation(lyr, 'INTERSECT', pt)
+        with arcpy.da.SearchCursor(lyr, [ field ]) as rows:
+            for r in rows:
+                attrs[field] = r[0]
+                break # in case there are more than one?
+    return attrs
+
+def get_range_and_parity(pt: Union[arcpy.PointGeometry, Feature], centerline: Union[int, Feature]) -> Munch:
+    attrs = dict(
+        parity = None,
+        to_address=None,
+        from_address=None,
+        address_prefix=None,
+        side=None
+    )
+    if isinstance(pt, Feature):
+        pt = pt.geometry
+
+    if isinstance(centerline, int):
+        schema = DataSchema(DataType.ROAD_CENTERLINE)
+        flds = FIELDS.STREET
+        where = f'{schema.oidField} = {centerline}'
+        fields = schema.fieldNames + ['SHAPE@']
+        with arcpy.da.SearchCursor(schema.table, fields, where) as rows:
+            for r in rows:
+                centerline = schema.fromRow(fields, r)
+        
+        # make sure we have a valid feature
+        if isinstance(centerline, Feature):
+            # angle = get_angle(centerline.geometry)
+            line = centerline.geometry
+            pq = line.queryPointAndDistance(pt)
+            side = 'R' if pq[-1] else 'L'
+            parity = centerline.get(f'Parity_{side}')
+            attrs['parity'] = parity
+            attrs['side'] = side
+            attrs['address_prefix'] = centerline.get(flds.ADDRESS_PREFIX_RIGHT if side == 'R' else flds.ADDRESS_PREFIX_LEFT)
+            attrs['to_address'] = centerline.get(flds.TO_ADDRESS_RIGHT if side == 'R' else flds.TO_ADDRESS_LEFT)
+            attrs['from_address'] = centerline.get(flds.FROM_ADDRESS_RIGHT if side == 'R' else flds.FROM_ADDRESS_LEFT)
+    
+    return munchify(attrs)
+
+
+def create_address_point(pg: arcpy.PointGeometry, centerlineOID: int, **kwargs):
+    """creates an address point
+
+    Args:
+        pg (arcpy.PointGeometry): [description]
+        centerlineOID (int): [description]
+    """
+    schema = DataSchema(DataType.ADDRESS_POINTS)
+    kwargs.update(**get_zip_code(pg))
+    kwargs.update(**get_city_limits(pg))
+    ft = schema.create_feature(pg, **kwargs)
+    merge_street_segment_attributes(ft, centerlineOID)
+    schema.calculate_custom_fields(ft)
+    schema.commit_features()
+    return ft, schema
 
 if __name__ == '__main__':
     # sample point 205 MAIN
---
+++ ilng911/utils/cursors.py
@@ -0,0 +1,243 @@
+import os
+import arcpy
+import warnings
+
+LAYER_TYPE = arcpy.mapping.Layer if hasattr(arcpy, 'mapping') else arcpy._mp.Layer
+TABLE_TYPE = arcpy.mapping.TableView if hasattr(arcpy, 'mapping') else arcpy._mp.Table
+
+def find_ws(path, ws_type=None, return_type=False):
+    """[summary]
+    
+    Args:
+        path (str): path to features or workspace
+        ws_type (str, optional):option to find specific workspace type (FileSystem|LocalDatabase|RemoteDatabase), can be a single string or list of these options.
+        return_type (bool, optional): option to return workspace type as well.  If this option is selected, a tuple
+            of the full workspace path and type are returned. Defaults to False.
+    
+    Returns:
+        str: the path to the workspace.  If the return_type is set to True, it will return a tuple of (path, workspaceType)
+    """
+    def find_existing(path):
+        if arcpy.Exists(path):
+            return arcpy.Describe(path).catalogPath
+        else:
+            if not arcpy.Exists(path):
+                return find_existing(os.path.dirname(path))
+
+    # try original path first
+    if isinstance(path, (LAYER_TYPE, TABLE_TYPE)):
+        path = path.dataSource
+
+    if os.sep not in str(path):
+        if hasattr(path, 'dataSource'):
+            path = path.dataSource
+        else:
+            path = arcpy.Describe(path).catalogPath
+
+    path = find_existing(path)
+
+    # first make sure it's not an "in_memory" workspace
+    if (path or '').startswith('in_memory'):
+        return ('in_memory', 'LocalDatabase') if return_type else 'in_memory'
+
+    desc = arcpy.Describe(path)
+    if not isinstance(ws_type, list) and ws_type:
+        ws_type = [ws_type]
+
+    if hasattr(desc, 'workspaceType'):
+        if ws_type:
+            if desc.workspaceType in ws_type:
+                if return_type:
+                    return (path, desc.workspaceType)
+                else:
+                    return path
+        else:
+            if return_type:
+                return (path, desc.workspaceType)
+            else:
+                return path
+
+    # search until finding a valid workspace
+    path = str(path)
+    split = list(filter(None, str(path).split(os.sep)))
+    if path.startswith('\\\\'):
+        split[0] = r'\\{0}'.format(split[0])
+
+    # find valid workspace
+    for i in range(1, len(split)):
+        sub_dir = os.sep.join(split[:-i])
+        desc = arcpy.Describe(sub_dir)
+        if hasattr(desc, 'workspaceType'):
+            if ws_type == desc.workspaceType:
+                if return_type:
+                    return (sub_dir, desc.workspaceType)
+                else:
+                    return sub_dir
+            else:
+                if return_type:
+                    return (sub_dir, desc.workspaceType)
+                else:
+                    return sub_dir
+
+
+class EditableCursorMixin(object):
+    indexLookup = {}
+
+    def set(self, row, **kwargs):
+        """convenience method for setting row values by field name, automatically
+        calls updateRow()
+        Required:
+            row -- row object (list of values) from cursor
+            **kwargs -- keyword arguments for setting row values
+        """
+        for k,v in kwargs.items():
+            indx = self.indexLookup.get(k)
+            if indx is not None:
+                row[indx] = v
+        self.updateRow(row)
+
+    def add(self, **kwargs):
+        """convenience method for adding row values by field name, will automatically form
+        list of values to insert (defaulting to all Null) and insert row.
+        Required:
+            **kwargs -- keyword arguments for setting row values
+        """
+        row = [None] * len(self.indexLookup.keys())
+        for k,v in kwargs.items():
+            indx = self.indexLookup.get(k)
+            if indx is not None:
+                row[indx] = v
+        self.insertRow(row)
+
+class NGUpdateCursor(arcpy.da.UpdateCursor, EditableCursorMixin):
+
+    def __init__(self, *args, **kwargs):
+        super(NGUpdateCursor, self).__init__(*args, **kwargs)
+        self.indexLookup = {f: i for i,f in enumerate(self.fields)}
+
+    def add(self, *args, **kwargs):
+        raise NotImplementedError('Update Cursor does not support inserting new records!')
+
+class NGInsertCursor(arcpy.da.InsertCursor, EditableCursorMixin):
+
+    def __init__(self, *args, **kwargs):
+        super(NGInsertCursor, self).__init__(*args, **kwargs)
+        self.indexLookup = {f: i for i,f in enumerate(self.fields)}
+
+    def set(self, *args, **kwargs):
+        raise NotImplementedError('Insert Cursor does not support Updating records!')
+
+
+class InsertCursor(object):
+    """wrapper clas for arcpy.da.InsertCursor, to automatically
+    implement editing (required for versioned data, and data with
+    geometric networks, topologies, network datasets, and relationship
+    classes"""
+    def __init__(self, *args, **kwargs):
+        """initiate wrapper class for update cursor.  Supported args:
+        in_table, field_names
+        """
+        self.args = args
+        self.kwargs = kwargs
+        self.edit = None
+        self.alreadyInEditSession = False
+        self.fields = self.kwargs.get('fields')
+        if not self.fields and len(self.args) >=2:
+            self.fields = self.args[1]
+        self.indexLookup = {f: i for i,f in enumerate(self.fields or [])}
+
+    def __enter__(self):
+        ws = None
+        if self.args:
+            ws = find_ws(self.args[0])
+        elif 'in_table' in self.kwargs:
+            ws = find_ws(self.kwargs['in_table'])
+
+        try:
+            self.edit = arcpy.da.Editor(ws)
+            self.edit.startEditing()
+            self.edit.startOperation()
+            self.cursor = NGInsertCursor(*self.args, **self.kwargs)
+            return self.cursor
+        except Exception as e:
+            # explicit check for active edit session, do not attempt starting new session
+            msg = ((hasattr(e,'message') and e.message == 'start edit session') or (hasattr(e,'msg') and e.msg == 'start edit session'))
+            if isinstance(e, RuntimeError) and msg:
+                self.cursor = NGInsertCursor(*self.args, **self.kwargs)
+                self.alreadyInEditSession = True
+                return self.cursor
+            else:
+                raise e
+
+    def __exit__(self, type, value, traceback):
+        if not self.alreadyInEditSession:
+            try:
+                self.edit.stopOperation()
+                self.edit.stopEditing(True)
+            except Exception as e:
+                warnings.warn("Exception On Insert Cursor! Records May Not Have Inserted: {}".format(e))
+                raise e
+    
+        self.edit = None
+        try:
+            del self.cursor
+        except:
+            pass
+
+
+class UpdateCursor(object):
+    """wrapper clas for arcpy.da.UpdateCursor, to automatically
+    implement editing (required for versioned data, and data with
+    geometric networks, topologies, network datasets, and relationship
+    classes"""
+    def __init__(self, *args, **kwargs):
+        """initiate wrapper class for update cursor.  Supported args:
+            in_table, field_names, where_clause=None, spatial_reference=None,
+            explode_to_points=False, sql_clause=(None, None)
+        """
+        self.args = args
+        self.kwargs = kwargs
+        self.edit = None
+        self.alreadyInEditSession = False
+        self.fields = self.kwargs.get('fields')
+        if not self.fields and len(self.args) >=2:
+            self.fields = self.args[1]
+        self.indexLookup = {f: i for i,f in enumerate(self.fields or [])}
+
+
+    def __enter__(self):
+        ws = None
+        if self.args:
+            ws = find_ws(self.args[0])
+        elif 'in_table' in self.kwargs:
+            ws = find_ws(self.kwargs['in_table'])
+
+        try:
+            self.edit = arcpy.da.Editor(ws)
+            self.edit.startEditing()
+            self.edit.startOperation()
+            self.cursor = NGUpdateCursor(*self.args, **self.kwargs)
+            return self.cursor
+        except Exception as e:
+            # explicit check for active edit session, do not attempt starting new session
+            msg = ((hasattr(e,'message') and e.message == 'start edit session') or (hasattr(e,'msg') and e.msg == 'start edit session'))
+            if isinstance(e, RuntimeError) and msg:
+                self.cursor = NGUpdateCursor(*self.args, **self.kwargs)
+                self.alreadyInEditSession = True
+                return self.cursor
+            else:
+                raise e
+
+    def __exit__(self, type, value, traceback):
+        if not self.alreadyInEditSession:
+            try:
+                self.edit.stopOperation()
+                self.edit.stopEditing(True)
+            except Exception as e:
+                warnings.warn("Exception On Update Cursor! Records May Not Have Updated: {}".format(e))
+            
+        self.edit = None
+        try:
+            del self.cursor
+        except:
+            pass
