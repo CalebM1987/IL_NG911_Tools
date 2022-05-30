65f5201eea863cdb188d1b526f1447c72553e0e8
Caleb Mackey <caleb.mackey@bolton-menk.com>
2022-05-29 20:09:44 -0500

    feat: use config file to find ng911 schemas

---
+++ ilng911/config/__init__.py
@@ -0,0 +1,39 @@
+import os
+from ..utils.json_helpers import load_json, write_json_file
+from ..support.munch import munchify, Munch
+from typing import Dict, Union
+
+thisDir = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_config(file_name='config.json') -> Munch:
+    """loads the config file
+
+    Args:
+        file_name (str, optional): the config file name. Defaults to 'config.json'.
+
+    Returns:
+        Munch: the config
+    """
+    config_file = os.path.join(thisDir, file_name)
+
+    if os.path.exists(config_file):
+        return load_json(config_file)
+
+    return None
+
+def write_config(config: Union[Dict, Munch], file_name='config.json'):
+    """_summary_
+
+    Args:
+        config (Union[Dict, Munch]): _description_
+        file_name (str, optional): _description_. Defaults to 'config.json'.
+    """
+    config_file = os.path.join(thisDir, file_name)
+
+    write_json_file(config, config_file)
+
+
+
+
+
--- ilng911/env.py
+++ ilng911/env.py
@@ -5,8 +5,17 @@ is_arc = os.path.basename(sys.executable).startswith('Arc')
 
 NG_911_DIR = os.path.abspath(os.path.dirname(__file__))
 from ilng911.core.database import NG911Data
+from ilng911.config import load_config
 
-try:
-    ng911_db = NG911Data('test')
-except Exception as e:
-    ng911_db = None
+def get_ng911_db(config_file='config.json') -> NG911Data:
+    """gets the NG911Data helper instance
+
+    Args:
+        config_file (str, optional): the config file name. Defaults to 'config.json'.
+
+    Returns:
+        NG911Data: the NG911Data helper instance
+    """
+    config = load_config(config_file)
+    if config:
+        return NG911Data(config.get('ng911GDBPath'))
