32bb75a61ec6a18e63931db2e3acc5af7c8deb19
Caleb Mackey <caleb.mackey@bolton-menk.com>
2022-05-30 15:37:24 -0500

    test: test update

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ import sys
 import arcpy
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from ilng911.admin.schemas import features_from_json, create_ng911_admin_gdb
-from ilng911.env import NG_911_DIR
+from ilng911.env import NG_911_DIR, get_ng911_db
 from ilng911.logging import log_context, log
 
 if __name__ == '__main__':
@@ -13,7 +13,12 @@ if __name__ == '__main__':
     gdbPath = r'L:\Users\caleb.mackey\Documents\GIS_Data\IL_NG911\IL_NG911_Brown_Master_v3.2.5.gdb'
 
     with log_context() as lc:
-        gdb = create_ng911_admin_gdb(gdbPath, schemaPath, 'BROWN')
+        ng911_db = get_ng911_db()
+        log(f'setup is complete? {ng911_db.setupComplete}')
+        if not ng911_db.setupComplete:
+            gdb = create_ng911_admin_gdb(gdbPath, schemaPath, 'BROWN')
+            ng911_db.setup()
+            log(f'created admin gdb, is setup complete? {ng911_db.setupComplete}')
 
         # for tab in tables:
         #     json_file = os.path.join(schemas_dir, tab + '.json')
